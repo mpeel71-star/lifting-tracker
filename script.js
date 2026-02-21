c8907c2a5adc70437a39bc738e173901e62873a8
mpeel71-star <mpeel71@gmail.com>
2026-02-20 17:59:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+alert("script.js is running");
 // ================= Firebase (Auth + Firestore) =================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import {
